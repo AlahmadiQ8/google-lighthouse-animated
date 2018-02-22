bbf84abfcba66df672607b61b1ecb3f8b4eb6b85
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-02-22 10:50:36 -0800

    Updates

--- dist/index.js
+++ dist/index.js
@@ -1,60 +1,53 @@
 'use strict';
 
-TweenMax.set('.cloud-small', {
-  visibility: 'visible',
-  x: '50%'
-});
+CSSPlugin.defaultTransformPerspective = 500;
+TweenMax.defaultTransformPerspective = 500;
 
-var CloudSmall = function CloudSmall() {
-  // TODO: cloud doesn't move exactly within `range`
-  // cloud moves between -range/2 and range/2
-  var range = 1300;
-  var tweenCloudSmall = TweenMax.to('.cloud-small', 40, {
-    x: '+=' + range,
+var WIDTH = 1135;
+var HEIGHT = 737;
+var NEGATIVE_OFFSET = 200;
+
+var cloudSmall = function cloudSmall() {
+  var tweenCloudSmall = TweenMax.to('#Cloud-1', 50, {
+    x: WIDTH,
     ease: Power0.easeNone,
-    modifiers: {
-      x: function x(_x) {
-        return _x % range - range / 2;
-      }
-    },
+    yoyo: true,
     repeat: -1
   });
   return tweenCloudSmall;
 };
 
 var largeCloud = function largeCloud() {
-  var initialPosition = 750;
-  var rightEdge = 1450;
-  return TweenMax.to('.cloud-large', 50, {
+  return TweenMax.to('#Cloud-3', 50, {
+    x: WIDTH,
     ease: Power0.easeNone,
-    x: '+=' + rightEdge,
-    modifiers: {
-      x: function x(_x2) {
-        return _x2 % (rightEdge + initialPosition) - initialPosition;
-      }
-    },
+    yoyo: true,
     repeat: -1
   });
 };
 
 var mediumCloud = function mediumCloud() {
-  var leftEdge = -1185;
-  var rightEdge = 125;
-  var range = rightEdge - leftEdge;
-  console.log(range);
-  var tweenMediumCloud = TweenMax.to('.cloud-medium', 60, {
-    x: '-=' + range,
+  var tweenMediumCloud = TweenMax.from('#Cloud-2', 60, {
+    x: WIDTH,
     ease: Power0.easeNone,
-    modifiers: {
-      x: function x(_x3) {
-        return _x3 % range + Math.abs(rightEdge);
-      }
-    },
+    yoyo: true,
     repeat: -1
   });
   return tweenMediumCloud;
 };
 
+var rayRotation = function rayRotation() {
+  var tweenRayRotation = TweenMax.to('#Ray', 3, {
+    transformPerspective: 2000,
+    rotationY: '-360',
+    // transformOrigin: '636px center',
+    transformOrigin: '636px center',
+    repeat: -1,
+    ease: Power0.easeNone
+  });
+  return tweenRayRotation;
+};
+
 var master = new TimelineMax();
 master.add('start');
-master.add(CloudSmall(), 'start').add(largeCloud(), 'start-=25').add(mediumCloud(), 'start+=10');
+master.add(cloudSmall(), 'start').add(largeCloud(), 'start-=15').add(mediumCloud(), 'start').add(rayRotation(), 'start');
--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
-TweenMax.set('.cloud-small', {
-  visibility: 'visible',
-  x: '50%',
-});
+CSSPlugin.defaultTransformPerspective = 500;
+TweenMax.defaultTransformPerspective = 500;
 
-const CloudSmall = () => {
-  // TODO: cloud doesn't move exactly within `range`
-  // cloud moves between -range/2 and range/2
-  const range = 1300;
-  const tweenCloudSmall = TweenMax.to('.cloud-small', 40, {
-    x: `+=${range}`,
+const WIDTH = 1135;
+const HEIGHT = 737;
+const NEGATIVE_OFFSET = 200;
+
+
+const cloudSmall = function() {
+  const tweenCloudSmall = TweenMax.to('#Cloud-1', 50, {
+    x: WIDTH,
     ease: Power0.easeNone,
-    modifiers: {
-      x: x => x % range - range / 2,
-    },
+    yoyo: true,
     repeat: -1,
   });
   return tweenCloudSmall;
 };
 
-const largeCloud = () => {
-  const initialPosition = 750;
-  const rightEdge = 1450;
-  return TweenMax.to('.cloud-large', 50, {
+const largeCloud = function() {
+  return TweenMax.to('#Cloud-3', 50, {
+    x: WIDTH,
     ease: Power0.easeNone,
-    x: `+=${rightEdge}`,
-    modifiers: {
-      x: x => x % (rightEdge + initialPosition) - initialPosition,
-    },
+    yoyo: true,
     repeat: -1,
   });
 };
 
-const mediumCloud = () => {
-  const leftEdge = -1185;
-  const rightEdge = 125;
-  const range = rightEdge - leftEdge;
-  console.log(range);
-  const tweenMediumCloud = TweenMax.to('.cloud-medium', 60, {
-    x: `-=${range}`,
+const mediumCloud = function() {
+  const tweenMediumCloud = TweenMax.from('#Cloud-2', 60, {
+    x: WIDTH,
     ease: Power0.easeNone,
-    modifiers: {
-      x: x => x % range +  Math.abs(rightEdge),
-    },
+    yoyo: true,
     repeat: -1,
   });
   return tweenMediumCloud;
 }
 
+const rayRotation = function() {
+  const tweenRayRotation = TweenMax.to('#Ray', 3, {
+    transformPerspective: 2000,
+    rotationY: '-360',
+    // transformOrigin: '636px center',
+    transformOrigin: '636px center',
+    repeat: -1,
+    ease: Power0.easeNone,
+  })
+  return tweenRayRotation;
+}
 
 const master = new TimelineMax();
 master.add('start');
 master
-  .add(CloudSmall(), 'start')
-  .add(largeCloud(), 'start-=25')
-  .add(mediumCloud(), 'start+=10');
+  .add(cloudSmall(), 'start')
+  .add(largeCloud(), 'start-=15')
+  .add(mediumCloud(), 'start')
+  .add(rayRotation(), 'start');
