b0741a7313d32c04e5541a267477f4ee4e68cf46
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-03-05 01:10:55 -0800

    Added rayrouce shadow animation

--- site/src/index.js
+++ site/src/index.js
@@ -40,15 +40,33 @@ const mediumCloud = function() {
 }
 
 const rayRotation = function() {
+  const transformOriginX = 548 + 180/2.0;
   const tweenRayRotation = TweenMax.to('#Ray', 3, {
-    transformPerspective: 2000,
+    transformPerspective: 3000,
     rotationY: '-360',
-    // transformOrigin: '636px center',
-    transformOrigin: '636px center',
     repeat: -1,
     ease: Power0.easeNone,
   })
   return tweenRayRotation;
+}
+
+
+const raySource = function() {
+  const el = document.getElementById('ray-source');
+  const maskEl = document.getElementById('ray-source-mask');
+  const diameter = el.getAttribute('rx');
+  const maskWidth = getWidth(maskEl);
+  const start = -diameter;
+  const end = +diameter + maskWidth;
+  TweenMax.set('#ray-source', { attr: { cx: end }, visibility: 'visible' });
+  const tween = TweenMax.to('#ray-source', 1.5, {
+    attr: { cx: `${start}` },
+    ease: Power0.easeNone,
+    repeat: -1,
+    repeatDelay: 1.5,
+  });
+  
+  return tween;
 }
 
 const master = new TimelineMax();
@@ -57,4 +75,5 @@ master
   .add(cloudSmall(), 'start')
   .add(largeCloud(), 'start-=15')
   .add(mediumCloud(), 'start')
-  .add(rayRotation(), 'start');
+  .add(rayRotation(), 'start')
+  .add(raySource(), 'start+=1.5')
