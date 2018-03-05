ed6ca756793bbbe72b5029cca337a3acc7904f31
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-03-05 01:11:53 -0800

    Updates

--- dist/index.js
+++ dist/index.js
@@ -7,9 +7,14 @@ var WIDTH = 1135;
 var HEIGHT = 737;
 var NEGATIVE_OFFSET = 200;
 
+var getWidth = function getWidth(el) {
+  return el.getBoundingClientRect().width;
+};
+
 var cloudSmall = function cloudSmall() {
+  var el = document.getElementById('Cloud-1');
   var tweenCloudSmall = TweenMax.to('#Cloud-1', 50, {
-    x: WIDTH,
+    x: WIDTH - getWidth(el),
     ease: Power0.easeNone,
     yoyo: true,
     repeat: -1
@@ -18,8 +23,9 @@ var cloudSmall = function cloudSmall() {
 };
 
 var largeCloud = function largeCloud() {
-  return TweenMax.to('#Cloud-3', 50, {
-    x: WIDTH,
+  var el = document.getElementById('Cloud-3');
+  return TweenMax.to(el, 50, {
+    x: WIDTH - getWidth(el),
     ease: Power0.easeNone,
     yoyo: true,
     repeat: -1
@@ -27,8 +33,9 @@ var largeCloud = function largeCloud() {
 };
 
 var mediumCloud = function mediumCloud() {
+  var el = document.getElementById('Cloud-2');
   var tweenMediumCloud = TweenMax.from('#Cloud-2', 60, {
-    x: WIDTH,
+    x: WIDTH - getWidth(el),
     ease: Power0.easeNone,
     yoyo: true,
     repeat: -1
@@ -37,17 +44,34 @@ var mediumCloud = function mediumCloud() {
 };
 
 var rayRotation = function rayRotation() {
+  var transformOriginX = 548 + 180 / 2.0;
   var tweenRayRotation = TweenMax.to('#Ray', 3, {
-    transformPerspective: 2000,
+    transformPerspective: 3000,
     rotationY: '-360',
-    // transformOrigin: '636px center',
-    transformOrigin: '636px center',
     repeat: -1,
     ease: Power0.easeNone
   });
   return tweenRayRotation;
 };
 
+var raySource = function raySource() {
+  var el = document.getElementById('ray-source');
+  var maskEl = document.getElementById('ray-source-mask');
+  var diameter = el.getAttribute('rx');
+  var maskWidth = getWidth(maskEl);
+  var start = -diameter;
+  var end = +diameter + maskWidth;
+  TweenMax.set('#ray-source', { attr: { cx: end }, visibility: 'visible' });
+  var tween = TweenMax.to('#ray-source', 1.5, {
+    attr: { cx: '' + start },
+    ease: Power0.easeNone,
+    repeat: -1,
+    repeatDelay: 1.5
+  });
+
+  return tween;
+};
+
 var master = new TimelineMax();
 master.add('start');
-master.add(cloudSmall(), 'start').add(largeCloud(), 'start-=15').add(mediumCloud(), 'start').add(rayRotation(), 'start');
+master.add(cloudSmall(), 'start').add(largeCloud(), 'start-=15').add(mediumCloud(), 'start').add(rayRotation(), 'start').add(raySource(), 'start+=1.5');
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ const WIDTH = 1135;
 const HEIGHT = 737;
 const NEGATIVE_OFFSET = 200;
 
+const getWidth = el => el.getBoundingClientRect().width;
 
 const cloudSmall = function() {
+  const el = document.getElementById('Cloud-1');
   const tweenCloudSmall = TweenMax.to('#Cloud-1', 50, {
-    x: WIDTH,
+    x: WIDTH - getWidth(el),
     ease: Power0.easeNone,
     yoyo: true,
     repeat: -1,
@@ -17,8 +19,9 @@ const cloudSmall = function() {
 };
 
 const largeCloud = function() {
-  return TweenMax.to('#Cloud-3', 50, {
-    x: WIDTH,
+  const el = document.getElementById('Cloud-3');
+  return TweenMax.to(el, 50, {
+    x: WIDTH - getWidth(el),
     ease: Power0.easeNone,
     yoyo: true,
     repeat: -1,
@@ -26,8 +29,9 @@ const largeCloud = function() {
 };
 
 const mediumCloud = function() {
+  const el = document.getElementById('Cloud-2');
   const tweenMediumCloud = TweenMax.from('#Cloud-2', 60, {
-    x: WIDTH,
+    x: WIDTH - getWidth(el),
     ease: Power0.easeNone,
     yoyo: true,
     repeat: -1,
@@ -36,15 +40,33 @@ const mediumCloud = function() {
 }
 
 const rayRotation = function() {
+  const transformOriginX = 548 + 180/2.0;
   const tweenRayRotation = TweenMax.to('#Ray', 3, {
-    transformPerspective: 2000,
+    transformPerspective: 3000,
     rotationY: '-360',
-    // transformOrigin: '636px center',
-    transformOrigin: '636px center',
     repeat: -1,
     ease: Power0.easeNone,
   })
   return tweenRayRotation;
+}
+
+
+const raySource = function() {
+  const el = document.getElementById('ray-source');
+  const maskEl = document.getElementById('ray-source-mask');
+  const diameter = el.getAttribute('rx');
+  const maskWidth = getWidth(maskEl);
+  const start = -diameter;
+  const end = +diameter + maskWidth;
+  TweenMax.set('#ray-source', { attr: { cx: end }, visibility: 'visible' });
+  const tween = TweenMax.to('#ray-source', 1.5, {
+    attr: { cx: `${start}` },
+    ease: Power0.easeNone,
+    repeat: -1,
+    repeatDelay: 1.5,
+  });
+  
+  return tween;
 }
 
 const master = new TimelineMax();
@@ -53,4 +75,5 @@ master
   .add(cloudSmall(), 'start')
   .add(largeCloud(), 'start-=15')
   .add(mediumCloud(), 'start')
-  .add(rayRotation(), 'start');
+  .add(rayRotation(), 'start')
+  .add(raySource(), 'start+=1.5')
