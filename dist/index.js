9d403a4aeafb272fde7548e5ce654179eb5ee1ca
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-03-07 01:47:26 -0800

    Updates

--- dist/index.js
+++ dist/index.js
@@ -43,9 +43,9 @@ var mediumCloud = function mediumCloud() {
   return tweenMediumCloud;
 };
 
-var rayRotation = function rayRotation() {
+var rayRotation = function rayRotation(duration) {
   var transformOriginX = 548 + 180 / 2.0;
-  var tweenRayRotation = TweenMax.to('#Ray', 3, {
+  var tweenRayRotation = TweenMax.to('#Ray', duration, {
     transformPerspective: 3000,
     rotationY: '-360',
     repeat: -1,
@@ -54,24 +54,54 @@ var rayRotation = function rayRotation() {
   return tweenRayRotation;
 };
 
-var raySource = function raySource() {
+var raySource = function raySource(duration) {
   var el = document.getElementById('ray-source');
   var maskEl = document.getElementById('ray-source-mask');
-  var diameter = el.getAttribute('rx');
+  var radius = el.getAttribute('rx');
   var maskWidth = getWidth(maskEl);
-  var start = -diameter;
-  var end = +diameter + maskWidth;
+  var start = -radius;
+  var end = +radius + maskWidth;
   TweenMax.set('#ray-source', { attr: { cx: end }, visibility: 'visible' });
-  var tween = TweenMax.to('#ray-source', 1.5, {
+  var tween = TweenMax.to('#ray-source', duration / 2, {
     attr: { cx: '' + start },
     ease: Power0.easeNone,
     repeat: -1,
-    repeatDelay: 1.5
+    repeatDelay: duration / 2
   });
 
   return tween;
 };
 
+var rayShadowBottom = function rayShadowBottom(duration) {
+  var el = document.getElementById('ray-shadow-bottom');
+  var maskEl = document.getElementById('balcony-mask');
+  var radius = el.getAttribute('r');
+  var maskWidth = getWidth(maskEl);
+  var start = -radius;
+  var end = +radius + maskWidth;
+  TweenMax.set(el, { attr: { cx: end }, visibility: 'visible' });
+  var tween = TweenMax.to(el, duration / 2, {
+    attr: { cx: '' + start },
+    ease: Power0.easeNone,
+    repeat: -1,
+    repeatDelay: duration / 2
+  });
+
+  return tween;
+};
+
+var rayScene = function rayScene() {
+  var tl = new TimelineMax();
+  var duration = 3;
+  tl.add('ray');
+  tl.add(rayRotation(duration), 'ray');
+  tl.add(raySource(duration), 'ray+=' + duration / 2);
+  tl.add(rayShadowBottom(duration), 'ray+=' + duration / 2);
+  return tl;
+};
+
 var master = new TimelineMax();
 master.add('start');
-master.add(cloudSmall(), 'start').add(largeCloud(), 'start-=15').add(mediumCloud(), 'start').add(rayRotation(), 'start').add(raySource(), 'start+=1.5');
+master.add(cloudSmall(), 'start').add(largeCloud(), 'start-=15').add(mediumCloud(), 'start').add(rayScene(), 'start');
+// .add(rayRotation(), 'start')
+// .add(raySource(), 'start+=1.5')
